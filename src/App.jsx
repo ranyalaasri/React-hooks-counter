a9cc2e09254f331cc47a13a0897c2601ace5437e
Ranya <laasriranya1@gmail.com>
2024-05-03 10:58:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+
+function App(){
+  return (
+  <Counter/>
+  )
+
+}
+export default App
---
+++ src/MyComponent.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react"
+
+function MyComponent(){
+
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0)
+    const [isEmployed, setIsEmployed] = useState(false)
+
+
+    const updateName = () => {
+        setName("Ranya")
+    }
+
+    const incrementAge = () => {
+        setAge(age + 1 );
+    }
+
+    const employedStatus = () => {
+        setIsEmployed(!isEmployed)
+
+    }
+    return ( <div>
+        <p>Name: {name}</p>
+        <button onClick={updateName}>Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Age</button>
+
+        <p>Is employed: {isEmployed ? "yes" : "No"}</p>
+        <button onClick={employedStatus}>Status</button>
+    </div>
+
+    )
+}
+export default MyComponent
